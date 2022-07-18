e4ea48877678bfd9975ef2c1c16c1daff5c390dd
Muhammad Shahmeer <91695394+MShahmeer@users.noreply.github.com>
2022-07-19 02:54:06 +0500

    Update FunctionCurring.js
    modified function curring code

--- AsynchronousJavaScript/FunctionCurring.js
+++ AsynchronousJavaScript/FunctionCurring.js
@@ -2,12 +2,7 @@
 
 // In other words when a function instead of taking all the arguments at one time, takes the first one and return a new function take the second one and return another new function and so on until all the arguments fullfilled
 
-const sum = (num1) => {
-  return (num2) => {
-    return (num3) => {
-      console.log(`Sum: ${num1 + num2 + num3}`);
-    };
-  };
-};
+const sum = (num1) => (num2) => (num3) =>
+  console.log(`Sum: ${num1 + num2 + num3}`);
 
 sum(3)(4)(3);

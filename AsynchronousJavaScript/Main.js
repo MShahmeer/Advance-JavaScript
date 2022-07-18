803045b711116ffc63eaef5fc0f6daecb543299e
Muhammad Shahmeer <91695394+MShahmeer@users.noreply.github.com>
2022-07-19 02:05:11 +0500

    event loop
    what is event loop and main.js containing the concept of how async js works

---
+++ AsynchronousJavaScript/Main.js
@@ -0,0 +1,15 @@
+//This is called AsynchronousJavaScript
+
+const fun2 = () => {
+  setTimeout(() => {
+    console.log("function 2 is called");
+  }, 2000);
+};
+
+const fun1 = () => {
+  console.log("function 1 is called");
+  fun2();
+  console.log("function 1 is called again");
+};
+
+fun1();
